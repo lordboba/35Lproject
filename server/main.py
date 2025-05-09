fd28e53309bc711ad3a4dabffd9624aa52c837c6
ethantran314 <ethan.tran.sd@gmail.com>
2025-05-08 20:54:13 -0700

    minimum viable database+API

--- server/main.py
+++ server/main.py
@@ -1,11 +1,171 @@
-from fastapi import FastAPI
-import uvicorn
+import os
+from typing import Optional, List
 
-app = FastAPI()
+from fastapi import FastAPI, Body, HTTPException, status
+from fastapi.responses import Response
+from pydantic import ConfigDict, BaseModel, Field, EmailStr
+from pydantic.functional_validators import BeforeValidator
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+from typing_extensions import Annotated
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+from bson import ObjectId
+import motor.motor_asyncio
+from pymongo import ReturnDocument
+
+
+app = FastAPI(
+    title="Card Game API",
+    summary="Test version",
+)
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+db = client.game
+user_collection = db.get_collection("users")
+
+# Represents an ObjectId field in the database.
+# It will be represented as a `str` on the model so that it can be serialized to JSON.
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+
+class UserModel(BaseModel):
+    """
+    Container for a single user record.
+    """
+
+    # The primary key for the UserModel, stored as a `str` on the instance.
+    # This will be aliased to `_id` when sent to MongoDB,
+    # but provided as `id` in the API requests and responses.
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    name: str = Field(...)
+    games: int = Field(...)
+    wins: int = Field(...)
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True
+    )
+
+
+class UpdateUserModel(BaseModel):
+    """
+    A set of optional updates to be made to a document in the database.
+    """
+
+    name: Optional[str] = None
+    games: Optional[int] = None
+    wins: Optional[int] = None
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        json_encoders={ObjectId: str}
+    )
+
+
+class UserCollection(BaseModel):
+    """
+    A container holding a list of `UserModel` instances.
+
+    This exists because providing a top-level array in a JSON response can be a [vulnerability](https://haacked.com/archive/2009/06/25/json-hijacking.aspx/)
+    """
+
+    users: List[UserModel]
+
+
+@app.post(
+    "/users/",
+    response_description="Add new user",
+    response_model=UserModel,
+    status_code=status.HTTP_201_CREATED,
+    response_model_by_alias=False,
+)
+async def create_user(user: UserModel = Body(...)):
+    """
+    Insert a new user record.
+
+    A unique `id` will be created and provided in the response.
+    """
+    new_user = await user_collection.insert_one(
+        user.model_dump(by_alias=True, exclude=["id"])
+    )
+    created_user = await user_collection.find_one(
+        {"_id": new_user.inserted_id}
+    )
+    return created_user
+
+
+@app.get(
+    "/users/",
+    response_description="List all users",
+    response_model=UserCollection,
+    response_model_by_alias=False,
+)
+async def list_users():
+    """
+    List all of the user data in the database.
+
+    The response is unpaginated and limited to 1000 results.
+    """
+    return UserCollection(users=await user_collection.find().to_list(1000))
+
+
+@app.get(
+    "/users/{id}",
+    response_description="Get a single user",
+    response_model=UserModel,
+    response_model_by_alias=False,
+)
+async def show_user(id: str):
+    """
+    Get the record for a specific user, looked up by `id`.
+    """
+    if (
+        user := await user_collection.find_one({"_id": ObjectId(id)})
+    ) is not None:
+        return user
+
+    raise HTTPException(status_code=404, detail=f"User {id} not found")
+
+
+@app.put(
+    "/users/{id}",
+    response_description="Update a user",
+    response_model=UserModel,
+    response_model_by_alias=False,
+)
+async def update_user(id: str, user: UpdateUserModel = Body(...)):
+    """
+    Update individual fields of an existing user record.
+
+    Only the provided fields will be updated.
+    Any missing or `null` fields will be ignored.
+    """
+    user = {
+        k: v for k, v in user.model_dump(by_alias=True).items() if v is not None
+    }
+
+    if len(user) >= 1:
+        update_result = await user_collection.find_one_and_update(
+            {"_id": ObjectId(id)},
+            {"$set": user},
+            return_document=ReturnDocument.AFTER,
+        )
+        if update_result is not None:
+            return update_result
+        else:
+            raise HTTPException(status_code=404, detail=f"User {id} not found")
+
+    # The update is empty, but we should still return the matching document:
+    if (existing_user := await user_collection.find_one({"_id": id})) is not None:
+        return existing_user
+
+    raise HTTPException(status_code=404, detail=f"User {id} not found")
+
+
+@app.delete("/users/{id}", response_description="Delete a user")
+async def delete_user(id: str):
+    """
+    Remove a single user record from the database.
+    """
+    delete_result = await user_collection.delete_one({"_id": ObjectId(id)})
+
+    if delete_result.deleted_count == 1:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    raise HTTPException(status_code=404, detail=f"User {id} not found")

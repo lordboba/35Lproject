07ac353e8d2595efb4aabf81d46f9513990c18ef
ethantran314 <ethan.tran.sd@gmail.com>
2025-05-15 22:41:23 -0700

    moved models to new file

--- server/main.py
+++ server/main.py
@@ -15,7 +15,9 @@ from bson import ObjectId
 import motor.motor_asyncio
 from pymongo import ReturnDocument
 import pymongo
+from models import *
 from dotenv import load_dotenv
+
 load_dotenv()
 
 app = FastAPI(
@@ -39,93 +41,6 @@ app.add_middleware(
 client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
 db = client.game
 user_collection = db.get_collection("users")
-
-# Represents an ObjectId field in the database.
-# It will be represented as a `str` on the model so that it can be serialized to JSON.
-PyObjectId = Annotated[str, BeforeValidator(str)]
-
-class CardModel(BaseModel):
-    """
-    Container for a single card.
-    """
-    rank: int = Field(...)
-    suit: int = Field(...)
-    model_config = ConfigDict(
-        populate_by_name=True,
-        arbitrary_types_allowed=True
-    )
-
-class TransactionModel(BaseModel):
-    """
-    Container for a single transaction.
-    """
-    sender: str = Field(...)
-    receiver: str = Field(...)
-    card: CardModel = Field(...)
-    success: bool = Field(...)
-    model_config = ConfigDict(
-        populate_by_name=True,
-        arbitrary_types_allowed=True
-    )
-
-class TurnModel(BaseModel):
-    """
-    Container for a single turn.
-    """
-    player: str = Field(...)
-    transactions: List[TransactionModel] = Field(default_factory=list)
-
-class UserModel(BaseModel):
-    """
-    Container for a single user record.
-    """
-    id: Optional[PyObjectId] = Field(alias="_id", default=None)
-    firebase_uid: str = Field(...)
-    name: Optional[str] = Field(default=None)
-    games: int = Field(default=0)
-    wins: int = Field(default=0)
-    username_set: bool = Field(default=False)
-
-    model_config = ConfigDict(
-        populate_by_name=True,
-        arbitrary_types_allowed=True,
-        json_encoders={ObjectId: str}
-    )
-
-class UpdateUserModel(BaseModel):
-    """
-    A set of optional updates to be made to a document in the database.
-    """
-
-    name: Optional[str] = None
-    games: Optional[int] = None
-    wins: Optional[int] = None
-    model_config = ConfigDict(
-        arbitrary_types_allowed=True,
-        json_encoders={ObjectId: str}
-    )
-
-
-class UserCollection(BaseModel):
-    """
-    A container holding a list of `UserModel` instances.
-
-    This exists because providing a top-level array in a JSON response can be a [vulnerability](https://haacked.com/archive/2009/06/25/json-hijacking.aspx/)
-    """
-
-    users: List[UserModel]
-
-
-class FirebaseUserRegistrationRequest(BaseModel):
-    firebase_uid: str = Field(...)
-
-class CheckUsernameResponse(BaseModel):
-    is_available: bool
-
-class CompleteRegistrationRequest(BaseModel):
-    firebase_uid: str = Field(...)
-    username: str = Field(..., min_length=3, max_length=20, pattern="^[a-zA-Z0-9_]+$")
-
 
 @app.post(
     "/users/initialize",
---
+++ server/models.py
@@ -0,0 +1,101 @@
+import os
+from typing import Optional, List, Dict
+
+from fastapi import FastAPI, Body, HTTPException, status
+from fastapi.responses import Response
+from pydantic import ConfigDict, BaseModel, Field, EmailStr
+from pydantic.functional_validators import BeforeValidator
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+from typing_extensions import Annotated
+
+from bson import ObjectId
+import motor.motor_asyncio
+from pymongo import ReturnDocument
+
+# Represents an ObjectId field in the database.
+# It will be represented as a `str` on the model so that it can be serialized to JSON.
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+class CardModel(BaseModel):
+    """
+    Container for a single card.
+    """
+    rank: int = Field(...)
+    suit: int = Field(...)
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True
+    )
+
+class TransactionModel(BaseModel):
+    """
+    Container for a single transaction.
+    """
+    sender: str = Field(...)
+    receiver: str = Field(...)
+    card: CardModel = Field(...)
+    success: bool = Field(...)
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True
+    )
+
+class TurnModel(BaseModel):
+    """
+    Container for a single turn.
+    """
+    player: str = Field(...)
+    transactions: List[TransactionModel] = Field(default_factory=list)
+
+class UserModel(BaseModel):
+    """
+    Container for a single user record.
+    """
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    firebase_uid: str = Field(...)
+    name: Optional[str] = Field(default=None)
+    games: int = Field(default=0)
+    wins: int = Field(default=0)
+    username_set: bool = Field(default=False)
+
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True,
+        json_encoders={ObjectId: str}
+    )
+
+class UpdateUserModel(BaseModel):
+    """
+    A set of optional updates to be made to a document in the database.
+    """
+
+    name: Optional[str] = None
+    games: Optional[int] = None
+    wins: Optional[int] = None
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        json_encoders={ObjectId: str}
+    )
+
+
+class UserCollection(BaseModel):
+    """
+    A container holding a list of `UserModel` instances.
+
+    This exists because providing a top-level array in a JSON response can be a [vulnerability](https://haacked.com/archive/2009/06/25/json-hijacking.aspx/)
+    """
+
+    users: List[UserModel]
+
+
+class FirebaseUserRegistrationRequest(BaseModel):
+    firebase_uid: str = Field(...)
+
+class CheckUsernameResponse(BaseModel):
+    is_available: bool
+
+class CompleteRegistrationRequest(BaseModel):
+    firebase_uid: str = Field(...)
+    username: str = Field(..., min_length=3, max_length=20, pattern="^[a-zA-Z0-9_]+$")

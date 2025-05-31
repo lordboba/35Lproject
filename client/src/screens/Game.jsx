0f725e43a9c1b8bfb76ee77fe61c542ce0577376
Prabhvir Babra <theprabab@gmail.com>
2025-05-31 15:55:51 -0700

    Init zim

--- client/src/screens/Game.jsx
+++ client/src/screens/Game.jsx
@@ -1,108 +1,114 @@
-import React, { useEffect, useRef } from 'react';
-import tableSVG from '../assets/table.svg';
-import tableedgeSVG from '../assets/tableedge.svg';
+import React, { StrictMode, Component } from "react";
+
+import { Frame, Circle, Dial, Emitter, Pen, MotionController, series, LabelOnArc } from "zimjs"
+
+class ZimFrame extends Component {
+  componentDidMount() {
+    this.frame = new Frame({
+      scaling: "zim",
+      width: 600,
+      height: 300,
+      color: lighter,
+      mouseMoveOutside: true,
+      ready: () => {
+        // ~~~~~~~~~~~~~~~~~~
+        // INTRO
+        // ZIM is a general canvas framework at https://zimjs.com
+        // Below we show a few features - but this is a small sampling
+        // ZIM also makes easy Puzzles, Parallax, Drawings, Connectors, HitTests,
+        // Sprites, Animations, Shaders, Effects, Games, Dialogs, Visualizations
+        // https://zimjs.com/editor - see ZAPPS section
+        // https://zimjs.com/examples for many examples
+        // https://zimjs.com/code for CDN templates without NPM
+
+
+        // ~~~~~~~~~~~~~~~~~~
+        // SHAPES
+        // ZIM has lots of shapes including Blob and Squiggle for free-form shapes
+        // We can add transform(), gesture() and 80 other methods - here we drag()
+        // normally, drag() objects come up on top but that would put the circle above the emitter
+        // ZIM has chaining for most methods
+        const circle = new Circle(50, purple)
+              .center()
+              .drag({ onTop: false })
+
+        // ~~~~~~~~~~~~~~~~~~
+        // ANIMATION
+        // ZIM has industry leading animation on par with GSAP and beyond
+        // with animation along user editable paths, sequencing and series, and much more
+        // and so easy to use - at 30% the code of HTML/CSS animations
+              .animate({
+                props: { color: red },
+                time: 2,
+                rewind: true,
+                loop: true
+              })
+
+
+        // ~~~~~~~~~~~~~~~~~~
+        // COMPONENTS
+        // ZIM has over 40 components - see https://zimjs.com/uiux.html
+        // and optional normal parameters or a config object - we call this ZIM DUO
+        const dial = new Dial({ min: 1, max: 3, step: 0, backgroundColor: blue })
+              .sca(.5)
+              .pos(20, 20, RIGHT, BOTTOM)
+              .change(() => { circle.sca(dial.currentValue) }) // could also use wire()
+
+
+        // ~~~~~~~~~~~~~~~~~~
+        // CONTROLS
+        // ZIM has dozens of controls like Tile, Wrapper, Layout, Pages, SoundWave, Parallax, Synth, etc.
+        // We show the Emitter and Pen here
+        const emitter = new Emitter({ startPaused: true })
+
+        circle.on("pressup", () => {
+          emitter.loc(circle).spurt(10)
+        })
+
+        // ZIM has dynamic parameters - we call this ZIM VEE
+        // An array [] picks randomly, or use a min, max {}, series or result of a function
+        // The pen lines can be dragged, double clicked or held to be deleted
+        // ZIM has all sorts of features for Art and Data Visualization
+        // see https://zimjs.com/art.html and https://zimjs.com/data.html
+        const pen = new Pen({ min: 10, max: 60 }, series(pink, white)).addTo().bot()
+
+        // MotionController controls movement of any DisplayObject in various ways:
+        // mousemove, pressmove, pressdrag, keydown, gamebutton, gamestick, swipe, follow, manual
+        // Note the mousedownIncludes parameter if wanting to draw on things
+        new MotionController(pen, "pressmove")
+
+
+        // ~~~~~~~~~~~~~~~~~~
+        // TEXT
+        // ZIM has Label(), LabelLetters(), LabelWords(), LabelOnPath(), TextInput()
+        // for various features with text
+        new LabelOnArc("DRAW ON BACKING", 12, undefined, dark)
+          .center()
+          .animate({ wait: 2, props: { alpha: 0 } })
+
+      }
+    })
+  }
+
+  componentWillUnmount() {
+    this.frame?.dispose()
+  }
+
+  render() {
+    return null
+  }
+}
 
 function Game() {
-  const canvasRef = useRef(null);
-
-  useEffect(() => {
-    // Load ZIM.js dynamically
-    const script = document.createElement('script');
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/zimjs/15.3.0/zim.min.js';
-    script.async = true;
-    
-    script.onload = () => {
-      // ZIM is now available on the window object
-      const { Frame, Rectangle } = window.zim;
-      
-      // Create the ZIM Frame
-      const frame = new Frame({
-        canvas: canvasRef.current,
-        scaling: "fit",
-        width: 800,
-        height: 600,
-        color: "#EEE",
-      });
-
-      frame.on("ready", () => {
-        const stage = frame.stage;
-        
-        // Add a green rectangle using ZIM
-        const rect = new Rectangle(150, 150, "green")
-          .pos(10, 10)
-          .addTo(stage);
-          
-        // Add a blue circle for variety
-        const circle = new window.zim.Circle(75, "blue")
-          .pos(200, 200)
-          .addTo(stage);
-
-        // Add the SVG table to the center of the canvas
-        const tableBitmap = new window.zim.Bitmap(tableSVG)
-          .center(stage)
-          .mov(0, 60)
-          .addTo(stage);
-
-        // Optionally, scale the table to fit nicely
-        //tableBitmap.sca(0.2); // Adjust scale as needed
-        
-        // Add some interactivity
-        rect.on("click", () => {
-          rect.animate({
-            props: { x: Math.random() * 600, y: Math.random() * 400 },
-            time: 1000,
-            ease: "backOut"
-          });
-        });
-        
-        circle.on("click", () => {
-          circle.animate({
-            props: { scaleX: 1.5, scaleY: 1.5 },
-            time: 500,
-            ease: "elasticOut",
-            rewind: true
-          });
-        });
-        
-        stage.update();
-      });
-    };
-
-    // Add script to head
-    document.head.appendChild(script);
-
-    // Cleanup function to remove script when component unmounts
-    return () => {
-      if (document.head.contains(script)) {
-        document.head.removeChild(script);
-      }
-    };
-  }, []);
-
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        width: "100%",
-        margin: 0,
-        padding: 0,
-        backgroundColor: "#ccc",
-      }}
-    >
-      <h2 style={{ marginBottom: "20px", color: "#333" }}>
-        ZIM.js with React - Click the shapes!
-      </h2>
-      <div style={{ position: "relative", width: 500, height: 500, marginBottom: 20 }}>
-        <img src={tableedgeSVG} width={600} height={500} alt="table edge" style={{ position: "absolute", left: -50
-          , top: 0, zIndex: 0 }} />
-        <img src={tableSVG} width={500} height={500} alt="table" style={{ position: "absolute", left: 0, top: 0, zIndex: 1 }} />
+    <>
+      <div>
+        <StrictMode>
+          <div id="zim" />
+        </StrictMode>
+        <ZimFrame />
       </div>
-      <canvas ref={canvasRef} id="canvas" />
-    </div>
+    </>
   );
 }
 
